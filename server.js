ae9f6b3e58bbd8c529143a6d687becde2399b45d
Joep Sales <info@incomtechdesign.com>
2022-05-09 23:07:07 +0200

    Added RabbitMQ listener

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const amqp = require('amqplib/callback_api');
 
-const PORT = 3001;
+const PORT = 3002;
 
-app.use(bodyParser.json());
+amqp.connect('amqps://lzvlbhtr:6cvrOb5ZwKBJ1bJJJ3OOMKESR0Jhoyd8@chinook.rmq.cloudamqp.com/lzvlbhtr', (error0, connection) => {
+    if (error0) {
+        throw error0
+    }
 
-// Main Endpoint
-app.get('/', (req, res) => {
-    res.send('This is the default endpoint for Content-MS.');
-});
+    connection.createChannel((error1, channel) => {
+        if (error1) {
+            throw error1
+        }
 
-app.listen(PORT, () => {
-    console.log(`Service running on ${PORT}.`);
+        channel.assertQueue('hello', { durable: false });
+
+
+
+        app.use(bodyParser.json());
+
+        // Main Endpoint
+        app.get('/', (req, res) => {
+            res.send('This is the default endpoint for Content-MS.');
+        });
+
+        channel.consume('hello', (msg) => {
+            console.log(msg.content.toString());
+        })
+
+        app.listen(PORT, () => {
+            console.log(`Service running on ${PORT}.`);
+        })
+    })
 })
 
