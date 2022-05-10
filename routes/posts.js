80a2b43c12962fba5eb94fe8c8c706909e9209d3
Joep Sales <info@incomtechdesign.com>
2022-05-10 22:50:30 +0200

    Created post to FB flow

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Post = require('../models/Post');
 const FbPost = require('../models/FbPost');
 const amqp = require('amqplib/callback_api');
+const axios = require('axios');
 
 require('dotenv').config({ path: require('find-config')('.env') })
 const router = express.Router();
@@ -29,20 +30,28 @@ amqp.connect('amqps://lzvlbhtr:6cvrOb5ZwKBJ1bJJJ3OOMKESR0Jhoyd8@chinook.rmq.clou
             res.send('This is the default endpoint for Content-MS.');
         });
 
+        // channel.consume('post_created', async (msg) => {
+        //     const eventPost = JSON.parse(msg.content.toString())
+        //     const post = new Post();
+        //     post.title = eventPost.title;
+        //     post.location = eventPost.location;
+        //     await post.save();
+        //     console.log('Post created')
+        // }, { noAck: true })
+        
         channel.consume('post_created', async (msg) => {
-            console.log(msg.content.toString());
             const eventPost = JSON.parse(msg.content.toString())
-            const post = new Post();
-            post.title = eventPost.title;
-            post.location = eventPost.location;
-            await post.save();
+            const post = new FbPost();
+            post.content = eventPost.content;
+            await axios.post(`https://graph.facebook.com/${pageId}/feed?message=${post.content}&access_token=${token}`, null)
+            // post.save(); 
             console.log('Post created')
         }, { noAck: true })
 
         channel.consume('post_deleted', async (msg) => {
             const post_id = msg.content.toString();
             await Post.deleteOne({post_id})
-            console.log('Post deleted')
+            console.log(`Post with id ${post_id} deleted.`);
         })
     })
 })
